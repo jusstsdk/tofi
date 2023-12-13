8a5f37dd2de50682e1f3b8e605beb004679e417f
Angelina <angelina2003t@gmail.com>
2023-12-14 02:17:45 +0300

    minor fix

--- online-banking/server/db.js
+++ online-banking/server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: 'postgres://postgres:jusstsdk@localhost:5432/online-banking' || process.env.POSTGRES_URL + "?sslmode=require"
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
 })
 
 
