abc84ef247ec3ca299b1187dadd34f4e34b3a863
Angelina <angelina2003t@gmail.com>
2023-12-13 18:35:22 +0300

    minor fixes

--- online-banking/server/db.js
+++ online-banking/server/db.js
@@ -2,12 +2,16 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 
+// const pool = new Pool({
+//     user: process.env.DB_USER || "postgres",
+//     host: process.env.DB_HOST || "localhost",
+//     database: process.env.DB_NAME || "online-banking",
+//     password: process.env.DB_PASSWORD || "jusstsdk",
+//     port: process.env.DB_PORT || 5432
+// })
+
 const pool = new Pool({
-    user: process.env.DB_USER || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "online-banking",
-    password: process.env.DB_PASSWORD || "jusstsdk",
-    port: process.env.DB_PORT || 5432
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
 })
 
 module.exports = pool
